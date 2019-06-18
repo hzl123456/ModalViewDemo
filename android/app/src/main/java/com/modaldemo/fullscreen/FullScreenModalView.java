b9c44fa58ab0dae140ee6cc3e4102d966fe9d5a9
请叫我百米冲刺 <1576044139@qq.com>
2019-06-18 11:44:19 +0800

    更新

--- android/app/src/main/java/com/modaldemo/fullscreen/FullScreenModalView.java
+++ android/app/src/main/java/com/modaldemo/fullscreen/FullScreenModalView.java
@@ -1,8 +1,3 @@
-/*
- * FullScreenModalView.java
- * Copyright 2019 Qunhe Tech, all rights reserved.
- * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
- */
 package com.modaldemo.fullscreen;
 
 import android.app.Activity;
